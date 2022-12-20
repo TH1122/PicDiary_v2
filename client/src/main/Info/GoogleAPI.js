a9ef7f76f1c90b7654f35943d3ce4cccc6d8e540
TaeHyung KIM <th1122kim@gmail.com>
2022-12-20 20:19:20 +0900

    feat: @react-oauth/googl, jwt-decode 설치 및 구글원탭로그인해 로컬스토리지에 사용자정보저장 기능 구현

--- client/src/main/Info/GoogleAPI.js
+++ client/src/main/Info/GoogleAPI.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 const GoogleAPIContainer = styled.div`
   display: flex;
@@ -8,7 +10,19 @@ const GoogleAPIContainer = styled.div`
 const GoogleAPI = () => {
   return (
     <>
-      <GoogleAPIContainer></GoogleAPIContainer>
+      <GoogleAPIContainer>
+        <GoogleOAuthProvider clientId="754572681344-7g2bod7bhfo6u6o5vkrpiid90frla2vu.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              let jwt = jwt_decode(JSON.stringify(credentialResponse));
+              localStorage.setItem("credentialResponse", JSON.stringify(jwt));
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      </GoogleAPIContainer>
     </>
   );
 };
