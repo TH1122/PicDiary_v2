f62f1f1f1d78d875d2878022866996b35c2bed1f
TaeHyung KIM <th1122kim@gmail.com>
2022-12-21 21:26:26 +0900

    feat: google photos api를 이용해, 서비스 기사용여부를 확인하기 위함 및 등록된 데이터 확인을 위해  picData 상태관리 기능 구현

--- client/src/main/Home/Home.js
+++ client/src/main/Home/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import StartPic from "./StartPic";
 
@@ -9,6 +10,24 @@ const HomeContainer = styled.div`
 `;
 const Home = () => {
   const [picData, setPicData] = useState([]);
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("oauth2-test-params"));
+    axios
+      .get("https://photoslibrary.googleapis.com/v1/albums", {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token["access_token"]}`,
+        },
+      })
+      .then((res) => {
+        return res.data.albums;
+      })
+      .then((data) => {
+        data.forEach((el) => {
+          if (el.title === "PicDiary") setPicData([el]);
+        });
+      });
+  }, []);
   return (
     <>
       <HomeContainer>{!picData.length && <StartPic />}</HomeContainer>
