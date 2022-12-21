fa4158a2ae7dcee5ab0ccdb73a9e30890dac4930
TaeHyung KIM <th1122kim@gmail.com>
2022-12-21 14:31:28 +0900

    feat: 로컬스토리지 내역을 확인해 로그아웃하기 전까지 새로고침해도 Home 화면이 렌더링 될 수 있도록 구현

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Nav from "./nav/Nav";
 import Main from "./main/Main";
 import Footer from "./footer/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("credentialResponse")) setIsLogin(true);
+  }, []);
   return (
     <>
       <div>
